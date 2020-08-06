d8f1f9c9103223c8cb6babb0d79612382722c92b
John Brunton <john_brunton@hotmail.co.uk>
2020-08-06 11:20:01 +0100

    add calculator screen to example

--- ExampleProject/App.js
+++ ExampleProject/App.js
@@ -1,22 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { fib } from 'TestReactComponentJS/src/fib'
+import { Button } from 'react-native';
+import { FibApp } from 'TestReactComponentJS'
 
-export default function App() {
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+const HomeScreen = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! hi! {fib(5)}</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Button
+      title="Launch Fib Calculator"
+      onPress={() =>
+        navigation.navigate('Calculator')
+      }
+    />
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen name="Calculator" component={FibApp} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
