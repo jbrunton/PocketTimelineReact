8631546c63485c8cfb62a5c28cd5d9ae8e455161
John Brunton <john_brunton@hotmail.co.uk>
2020-08-06 10:07:15 +0100

    use test component in example

--- ExampleProject/App.js
+++ ExampleProject/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { fib } from 'TestReactComponentJS/src/fib'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! hi! {fib(5)}</Text>
       <StatusBar style="auto" />
     </View>
   );
